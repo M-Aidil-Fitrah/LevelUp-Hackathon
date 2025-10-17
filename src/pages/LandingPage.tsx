8382639939ce0eb38636597bb915cd41b10cc4ae
Muhammad Aidil Fitrah <muh.aidil@mhs.usk.ac.id>
2025-10-17 21:14:51 +0700

    feat: add navbar

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,32 @@
 import Hero from "@/components/Hero";
+import PillNav from "@/components/PillNav";
+import { useLocation } from "react-router-dom";
 
 const LandingPage = () => {
+      const location = useLocation();
   return (
+    <div className="min-h-screen bg-white">
+      <PillNav
+        logo={"/LevelUp_logo.svg"}
+        logoAlt="LevelUp Logo"
+        items={[
+          { label: 'Home', href: '/' },
+          { label: 'Marketplace', href: '/marketplace' },
+          { label: 'UMKM Nearby', href: '/umkm-nearby' },
+          { label: 'Register', href: '/register', variant: 'accent' },
+          { label: 'Login', href: '/login', variant: 'accent' }
+        ]}
+        activeHref={location.pathname}
+        className="custom-nav"
+        ease="power2.easeOut"
+        baseColor="#000000"
+        pillColor="#ffffff"
+        hoveredPillTextColor="#ffffff"
+        pillTextColor="#000000"
+        accentColor="#FF2000"
+      />
     <Hero />
+    </div>
   );
 };
 
